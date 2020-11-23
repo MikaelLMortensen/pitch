bef80eb8147f9bc20d642ba3bcc8378c2470da2b
MikaelLMortensen <MikaelLMortensen@users.noreply.github.com>
2020-11-23 20:45:10 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,67 +1,51 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("FR", 50)
+    enabled = 1
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendValue("FL", 0)
-    radio.sendValue("FR", 0)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendValue("FL", 50)
-})
-radio.onReceivedValue(function (name, value) {
-    basic.showString(name +  ":" + value.toString())
+   radio.sendString("0;0")
+    enabled = 0
 })
 
-let pitch = 0
-let absPitch = 0
-let absRoll = 0
-let leftSpeed = 0
-let rightSpeed = 0
+radio.onReceivedString(function (receivedString) {
+	basic.showString(receivedString)
+})
+
+input.onButtonPressed(Button.B, function () {
+   radio.sendString("100;100")
+})
+
+let enabled = 1
 let oldRoll = 0
 let oldPitch = 0
+let absPitch = 0
+let absRoll = 0
+let pitch = 0
+let leftSpeed = 0
+let rightSpeed = 0
 radio.setGroup(256)
 basic.forever(function () {
     absRoll = input.rotation(Rotation.Roll)
     absPitch = input.rotation(Rotation.Pitch)
 
-    if (oldPitch != absPitch || oldRoll != absRoll)
-    {
-        oldPitch = absPitch 
+    if (enabled==1 && (oldPitch != absPitch || oldRoll != absRoll)) {
+        oldPitch = absPitch
         oldRoll = absRoll
-
-        leftSpeed = absPitch - absRoll
-        rightSpeed = absPitch + absRoll
-
-        if (leftSpeed != 0){
-            if (leftSpeed >= 0) {
-                if (leftSpeed > 100){
-                    leftSpeed = 100
-                }
-                radio.sendValue("FL", leftSpeed)
-                basic.showString("FL:" + leftSpeed.toString())
-            } else {
-                if (leftSpeed < -100){
-                    leftSpeed = -100
-                }
-                leftSpeed = leftSpeed * - 1
-                radio.sendValue("BL", leftSpeed)
-                basic.showString("BL:" + leftSpeed.toString())
-            }
-            if (rightSpeed >= 0) {
-                if (rightSpeed > 100){
-                    rightSpeed = 100
-                }
-                radio.sendValue("FR", rightSpeed)
-                basic.showString("FR:" + rightSpeed.toString())
-            } else {
-                if (rightSpeed < -100){
-                    rightSpeed = -100
-                }
-                rightSpeed = rightSpeed * - 1
-                radio.sendValue("BR", rightSpeed)
-                basic.showString("BR:" + rightSpeed.toString())
-            }
-            basic.pause(1000)
+        leftSpeed = absPitch - Math.round(absRoll / 5)
+        if (leftSpeed > 100){
+            leftSpeed = 100
         }
-    } 
+        if (leftSpeed < 0){
+            leftSpeed = 0
+        }
+
+        rightSpeed = absPitch + Math.round(absRoll / 5)
+        if (rightSpeed > 100) {
+            rightSpeed = 100
+        }
+        if (rightSpeed < 0) {
+            rightSpeed = 0
+        }
+        radio.sendString("" + leftSpeed.toString() + ";" + rightSpeed.toString())
+        basic.pause(50)
+    }
 })
